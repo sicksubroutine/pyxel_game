6571e497b12cea9cdfd431525602d21d9bc69f3a
Chaz <790@sicksubroutine.com>
2024-01-21 23:13:47 -0800

    animation lol

--- systems/animation_system.py
+++ systems/animation_system.py
@@ -8,9 +8,20 @@ class AnimationSystem(es.Processor):
     def __init__(self, game):
         self.game = game
         self.logger: Logger = game.logger
+        self.timer = 0.0
 
     def process(self):
+        self.timer += 1.0
         for entity, (animation, sprite) in es.get_components(Animation, Sprite):
+            if animation.looping:
+                if self.timer > animation.frame_rate:
+                    animation.current_frame += 1
+            else:
+                if self.timer > animation.frame_rate:
+                    animation.current_frame += 1
+                    if animation.current_frame == animation.num_frames:
+                        animation.current_frame = animation.num_frames - 1
+
             sprite.u = animation.frames[animation.current_frame].u
             sprite.v = animation.frames[animation.current_frame].v
             sprite.img = animation.frames[animation.current_frame].img
