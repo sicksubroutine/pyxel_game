a3e836b4e64b86b5de488a9878255ac6ca0aa156
Chaz <790@sicksubroutine.com>
2024-01-30 20:07:54 -0800

    blah blah blah

--- spawn_builder.py
+++ spawn_builder.py
@@ -11,10 +11,19 @@ from misc.config import ConfigManager
 from misc.logger import Logger
 from misc.asset_store import AssetStore
 from misc.entity import EntityPool
+from dataclasses import dataclass
 
 from level_loader.enemies import Enemies
 
 from systems.star_system import StarSystem
+
+
+@dataclass
+class Rect:
+    x: int
+    y: int
+    w: int
+    h: int
 
 
 class BaseMode:
@@ -26,11 +35,50 @@ class BaseMode:
     def update(self): ...
 
 
+class SelectionMode(BaseMode):
+    def __init__(self, tool):
+        self.tool = tool
+        self.potential_ships = self.tool.potential_ships
+        self.selected_ship = tool.selected_ship
+
+    def render(self):
+        px.text(0, 0, "Selection Mode", 7)
+        for index, ship in enumerate(self.potential_ships):
+            px.blt(
+                10,
+                10 + (10 * index),
+                0,
+                self.potential_ships[ship]["u"],
+                self.potential_ships[ship]["v"],
+                8,
+                8,
+                0,
+            )
+
+    def update(self):
+        mouse_pos = vec2(px.mouse_x, px.mouse_y)
+        if px.btnp(px.MOUSE_BUTTON_LEFT):
+            for ship in self.potential_ships:
+                if (
+                    self.potential_ships[ship]["location_on_screen"].x
+                    < mouse_pos.x
+                    < self.potential_ships[ship]["location_on_screen"].x
+                    + self.potential_ships[ship]["location_on_screen"].w
+                    and self.potential_ships[ship]["location_on_screen"].y
+                    < mouse_pos.y
+                    < self.potential_ships[ship]["location_on_screen"].y
+                    + self.potential_ships[ship]["location_on_screen"].h
+                ):
+                    print(f"Selected ship: {ship}")
+                    self.tool.selected_ship = ship
+                    self.tool.logger.Log(f"Selected ship: {self.selected_ship}")
+
+
 class PlacementMode(BaseMode):
     def __init__(self, tool):
         self.tool = tool
         self.potential_ships = self.tool.potential_ships
-        self.selected_ship = None
+        self.selected_ship = tool.selected_ship
 
     def render(self):
         px.text(0, 0, "Placement Mode", 7)
@@ -46,24 +94,7 @@ class PlacementMode(BaseMode):
                 0,
             )
 
-    def update(self):
-        # check if ship is selected using mouse and location on screen
-        for index, ship in enumerate(self.potential_ships):
-            if (
-                self.tool.mouse_pos.x
-                > self.potential_ships[ship]["location_on_screen"].x
-                and self.tool.mouse_pos.x
-                < self.potential_ships[ship]["location_on_screen"].x + 8
-                and self.tool.mouse_pos.y
-                > self.potential_ships[ship]["location_on_screen"].y
-                and self.tool.mouse_pos.y
-                < self.potential_ships[ship]["location_on_screen"].y + 8
-            ):
-                self.selected_ship = ship
-                print(f"Selected ship: {self.selected_ship}")
-                break
-            else:
-                self.selected_ship = None
+    def update(self): ...
 
 
 class PreviewMode(BaseMode):
@@ -98,13 +129,14 @@ class SpawnBuilder:
         self.spawner = Spawner(self)
         self.enemies: Enemies = Enemies(self)
         self.pool: EntityPool = EntityPool(self)
+        self.selected_ship = None
 
         self.potential_ships = {
-            "ship1": {"u": 8, "v": 0, "location_on_screen": vec2(10, 10)},
-            "ship2": {"u": 8, "v": 8, "location_on_screen": vec2(10, 20)},
-            "ship3": {"u": 8, "v": 16, "location_on_screen": vec2(10, 30)},
-            "ship4": {"u": 8, "v": 24, "location_on_screen": vec2(10, 40)},
-            "ship5": {"u": 8, "v": 32, "location_on_screen": vec2(10, 50)},
+            "ship1": {"u": 8, "v": 0, "location_on_screen": Rect(10, 10, 8, 8)},
+            "ship2": {"u": 8, "v": 8, "location_on_screen": Rect(10, 20, 8, 8)},
+            "ship3": {"u": 8, "v": 16, "location_on_screen": Rect(10, 30, 8, 8)},
+            "ship4": {"u": 8, "v": 24, "location_on_screen": Rect(10, 40, 8, 8)},
+            "ship5": {"u": 8, "v": 32, "location_on_screen": Rect(10, 50, 8, 8)},
         }
 
         self.enemy_types = self.enemies.enemy_types
@@ -119,41 +151,54 @@ class SpawnBuilder:
         self.systems_setup()
         self.delay = 0.0
 
+        # show mouse
+        px.mouse(True)
+
         # possible modes: placement, preview
-        self.possible_modes = ["placement", "preview"]
-        self.tool_mode = self.possible_modes[0]  # Starting mode is placement
+        self.possible_modes = [
+            {
+                "name": "selection",
+                "class": SelectionMode,
+            },
+            {
+                "name": "placement",
+                "class": PlacementMode,
+            },
+            {
+                "name": "preview",
+                "class": PreviewMode,
+            },
+        ]
+        self.mode_index = 0
+        self.tool_mode = self.possible_modes[self.mode_index]
+        self.tool_class = self.possible_modes[self.mode_index]["class"](self)
 
-        self.placement_mode = PlacementMode(self)
-        self.preview_mode = PreviewMode(self)
-        self.tool_class = self.placement_mode
+    def mode_class_switch(self):
+        self.tool_mode = self.possible_modes[self.mode_index]
+        self.tool_class = self.possible_modes[self.mode_index]["class"](self)
 
-    def mode_switcher(self, current_mode):
+    def mode_switcher(self):
         # change mode class
         try:
-            if current_mode == self.possible_modes[0]:
-                self.tool_class = self.placement_mode
-            elif current_mode == self.possible_modes[1]:
-                self.tool_class = self.preview_mode
-            else:
-                raise ValueError(f"Unknown mode: {current_mode}")
+            self.mode_index += 1
+            if self.mode_index > len(self.possible_modes) - 1:
+                self.mode_index = 0
         except Exception as e:
             self.logger.Log(f"Error: {e}")
 
     def keyboard_controls(self):
         # switch mode using m key
         if px.btnp(px.KEY_M):
-            if self.tool_mode == self.possible_modes[0]:
-                self.tool_mode = self.possible_modes[1]
-                self.mode_switcher(self.tool_mode)
-            else:
-                self.tool_mode = self.possible_modes[0]
-                self.mode_switcher(self.tool_mode)
+            self.mode_switcher()
+        # print out the current selected ship if press p
+        if px.btnp(px.KEY_P):
+            print(f"Selected ship: {self.selected_ship}")
 
     def mouse_controls(self):
         self.mouse_pos = vec2(px.mouse_x, px.mouse_y)
-        # center mouse on screen
-        px.mouse_x = self.res_width // 2
-        px.mouse_y = self.res_height // 2
+        # # center mouse on screen
+        # px.mouse_x = self.res_width // 2
+        # px.mouse_y = self.res_height // 2
 
     def fps_counter(self):
         self.frames += 1
@@ -173,7 +218,10 @@ class SpawnBuilder:
         self.star_system = StarSystem(star_colors)
 
     def update(self):
+        self.current_mode_update = self.tool_class.update()
+        self.mode_class_switch()
         self.star_system.update()
+        # self.mouse_controls()
         self.keyboard_controls()
 
     def render(self):
@@ -181,8 +229,6 @@ class SpawnBuilder:
         self.star_system.render()
         # print the current mode on the screen
         self.current_mode_render = self.tool_class.render()
-        # render a box where the mouse is
-        px.rectb(px.mouse_x - 8, px.mouse_y - 8, 4, 4, 7)
 
     def run(self):
         px.run(self.update, self.render)
